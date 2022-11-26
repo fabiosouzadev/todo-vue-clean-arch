59ad99a22217d7467ab3e3f337e18dd322c48219
Fabio Souza <fabiovanderlei.developer@gmail.com>
2022-11-26 11:16:40 -0300

    chore: initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+
+const todos: any = reactive([
+  { description: "Estudar Typescript", done: true },
+  { description: "Fazer prova online", done: false },
+  { description: "Cortar a grana", done: false },
+]);
+const description = ref("");
+function addItem (){
+    todos.push({ description: description.value, done: false});
+    description.value = "";
+}
+</script>
+
+<template>
+  <div v-for="item in todos">
+    <span :style="{ 'text-decoration': item.done ? 'line-through' : '' }">{{ item.description }} {{ item.done }}</span>
+  </div>
+  <input type="text" v-model="description" @keyup.enter="addItem()"> 
+</template>
+
+<style scoped></style>
