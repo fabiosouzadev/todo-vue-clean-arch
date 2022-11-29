98993db2832e41755f77094edffc429039a4fc0d
Fabio Souza <fabiovanderlei.developer@gmail.com>
2022-11-29 19:56:29 -0300

    feat: add backend data

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,33 +1,36 @@
 <script setup lang="ts">
-import { reactive, ref } from "vue";
-const todos: any = reactive([
-  { description: "Estudar Typescript", done: true },
-  { description: "Fazer prova online", done: false },
-  { description: "Cortar a grana", done: false },
-]);
+import { onMounted, reactive, ref } from "vue";
+import axios from "axios";
+const data: any = reactive({ todos: [] });
 const description = ref("");
 function addItem() {
   // se for vazio
   if (!description.value) return;
   // se for duplicado
-  if (todos.some((item: any) => item.description === description.value)) return;
+  if (data.todos.some((item: any) => item.description === description.value))
+    return;
   // se ja tiverem 5 itens não resolvidos
-  if (todos.filter((item: any) => !item.done).length > 4) return;
-  todos.push({ description: description.value, done: false });
+  if (data.todos.filter((item: any) => !item.done).length > 4) return;
+  data.todos.push({ description: description.value, done: false });
   description.value = "";
 }
 function removeItem(item: any): void {
-  todos.splice(todos.indexOf(item), 1);
+  data.todos.splice(data.todos.indexOf(item), 1);
 }
 
 function toggleDone(item: any) {
   item.done = !item.done;
 }
+
+onMounted(async () => {
+  const response = await axios.get("http://localhost:3000/todos");
+  data.todos = response.data;
+});
 </script>
 
 <template>
-  <div v-if="todos.length === 0">No Item</div>
-  <div v-for="item in todos">
+  <div v-if="data.todos.length === 0">No Item</div>
+  <div v-for="item in data.todos">
     <span :style="{ 'text-decoration': item.done ? 'line-through' : '' }">
       {{ item.description }} {{ item.done }}
     </span>
